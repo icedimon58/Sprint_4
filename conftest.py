064c3c479b7ae398472578d1431b8bdea4c3c219
Osmolovskiy Dmitiy <icedimon@mail.ru>
2024-09-16 02:45:09 +0200

    edit tests

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from main import BooksCollector
 
 
 @pytest.fixture()
-def Set_books_collection():
+def set_books_collection():
     collector = BooksCollector()
     for i in books:
         collector.add_new_book(i[0])
@@ -13,7 +13,7 @@ def Set_books_collection():
 
 
 @pytest.fixture()
-def Del_favorite():
+def del_favorite():
     collector = BooksCollector()
     for i in books:
         collector.add_new_book(i[0])
